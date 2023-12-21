746b7897bb5cc93bfd056f1376f6d52ca28c264b
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2023-12-21 16:35:27 +0500

    fix: chart socket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Controls from "@/components/Controls";
 import { fetchChartData } from "@/utils/getChartData";
 import { useEffect, useState } from "react";
 import { ChartData, OrderType } from "@/types";
+import { Time } from "lightweight-charts";
 
 export default function Home() {
     const [symbol, setSymbol] = useState("BTCUSDT");
@@ -19,29 +20,42 @@ export default function Home() {
 
     useEffect(() => {
         const socket = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@trade`);
-        const socket2 = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@kline_${interval}`);
-        socket2.onmessage = (event) => {
-            const trade = JSON.parse(event.data);
-            const time = trade.E;
-            const open = parseFloat(trade.k.o);
-            const high = parseFloat(trade.k.h);
-            const low = parseFloat(trade.k.l);
-            const close = parseFloat(trade.k.c);
-            if (chartData[chartData.length - 1].open !== open) {
-                setChartData((pre) => [
-                    ...pre.slice(1),
-                    {
-                        time: time,
-                        open: open,
-                        high: high,
-                        low: low,
-                        close: close,
-                    },
-                ]);
-            }
-        };
+        // const socket2 = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@kline_${interval}`);
+        // socket2.onmessage = (event) => {
+        //     const trade = JSON.parse(event.data);
+        //     const time = trade.E;
+        //     const open = parseFloat(trade.k.o);
+        //     const high = parseFloat(trade.k.h);
+        //     const low = parseFloat(trade.k.l);
+        //     const close = parseFloat(trade.k.c);
+        //     if (chartData[chartData.length - 1].open !== open) {
+        //         setChartData((pre) => [
+        //             ...pre.slice(1),
+        //             {
+        //                 time: time,
+        //                 open: open,
+        //                 high: high,
+        //                 low: low,
+        //                 close: close,
+        //             },
+        //         ]);
+        //     }
+        // };
         socket.onmessage = (event) => {
             const trade = JSON.parse(event.data);
+            const updatedChartData = [
+                ...chartData.slice(4),
+                {
+                    time: trade.T,
+                    open: parseFloat(trade.p),
+                    high: parseFloat(trade.p),
+                    low: parseFloat(trade.p),
+                    close: parseFloat(trade.p),
+                },
+            ];
+            if (chartData[chartData.length - 1].open !== parseFloat(trade.p)) {
+                setChartData(updatedChartData);
+            }
             if (trade.m) {
                 setOrders((prev) => [
                     ...prev,
@@ -67,7 +81,7 @@ export default function Home() {
 
         return () => {
             socket.close();
-            socket2.close();
+            // socket2.close();
         };
     }, [chartData, interval, symbol]);
 
